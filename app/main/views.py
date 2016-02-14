785a26f9b349563c71343afd6631839ff3f6ff62
wadefletcher <wbfletch@gmail.com>
2016-02-13 21:39:55 -0500

    fixed db interface

--- app/main/views.py
+++ app/main/views.py
@@ -14,12 +14,13 @@ def before_request():
         cookie = ''.join(choice(ascii_uppercase) for i in range(32))
         resp = make_response(redirect(url_for('main.index', id=cookie)))
         resp.set_cookie('user_id', cookie)
-        u = User(id=cookie)
-        u.save()
         return resp
 
 @main.route('/')
 def index():
     id = request.cookies.get('user_id')
-    user = User.query.filter(User.id == id).first_or_404()
-    return render_template('index.html', id=id, user=user)
+    if User.query.filter(User.id == id).first() is None:
+        u = User(id=id)
+        u.save()
+    u = User.query.filter(User.id == id).first()
+    return render_template('index.html', id=id, user=u)
